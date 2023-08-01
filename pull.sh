c0561e2a2da117f7dabcc40c2147e454308ee19c
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-08-01 15:14:17 -0400

    Update pull to force overwrite

--- pull.sh
+++ pull.sh
@@ -2,7 +2,8 @@
 cd /home/potato
 if [ -d "NFC_Tracking" ]; then
     cd NFC_Tracking
-    git pull
+    git fetch --all
+    git reset --hard origin/master
 else
     git clone https://github.com/NxFerrara/NFC_Tracking.git
     cd NFC_Tracking
