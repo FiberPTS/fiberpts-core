1c35102fb6a9f259802fe07894e228af53c0a776
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-08-01 15:21:59 -0400

    Update Pull with correct origin

--- pull.sh
+++ pull.sh
@@ -3,7 +3,7 @@ cd /home/potato
 if [ -d "NFC_Tracking" ]; then
     cd NFC_Tracking
     git fetch --all
-    git reset --hard origin/master
+    git reset --hard origin/main
 else
     git clone https://github.com/NxFerrara/NFC_Tracking.git
     cd NFC_Tracking
