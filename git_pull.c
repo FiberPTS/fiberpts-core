6e1f4bd4c5722770f55fd9eea38e356798fec1fb
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-08-02 17:26:13 -0400

    Update git_pull.c

--- git_pull.c
+++ git_pull.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main() {
-    system("sh ~/NFC_Tracking/pull.sh");
-    system("sh ~/NFC_Tracking/compile_all.sh");
-    system("sh ~/NFC_Tracking/update_permissions.sh");
+    system("sh /home/potato/NFC_Tracking/pull.sh");
+    system("sh /home/potato/NFC_Tracking/compile_all.sh");
+    system("sh /home/potato/NFC_Tracking/update_permissions.sh");
     return 0;
 }
