419561f37f01df1103bbd1aa41f4f60188fdd694
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-08-18 13:53:57 -0400

    Update screen.py

--- screen.py
+++ screen.py
@@ -335,7 +335,8 @@ def main():
                     raw_data = convert_to_rgb565(image)
                     write_to_framebuffer(raw_data)
 
-            time.sleep(5)
+            time.sleep(0.5)
+            reset_screen_image(res, color="blue")
             pingTime += 1
             if pingTime >= 120:
                 send_sqs_message(machine_id, "Ping", "None", formatted_time_sec)
