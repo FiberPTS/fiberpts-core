1f498e29feae24e833f9a697997eaba66ecfc2e8
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-08-27 15:25:22 -0400

    Changed ec2 git pull

--- git_pull_ec2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    system("sh /home/potato/NFC_Tracking/pull.sh");
-    system("sh /home/potato/NFC_Tracking/update_permissions.sh");
-    return 0;
-}
---
+++ git_pull_ec2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Run pull.sh
+sh $HOME/NFC_Tracking/pull.sh
+
+# Run update_permissions.sh
+sh $HOME/NFC_Tracking/update_permissions.sh
