2ed29479151f96c3f70200d075f5ccf57ab8878c
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-08-28 16:58:58 -0400

    Update processAPI.py

--- processAPI.py
+++ processAPI.py
@@ -44,7 +44,7 @@ def handle_ip_request(req):
         # Extract the data from the request
         data_str = req.get('Data', '')
         data_json = json.loads(data_str)
-
+        print(data_json)
         # URL for updating a record
         url_update = f"{AIRTABLE_API_URL}tblFOfDowcZNlPRDL/{data_json.get('machine_record_id', ' ')}"
 
