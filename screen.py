7c5fc3b55a186da97365ede7e8c9f514652c4c36
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-08-28 14:49:24 -0400

    Update screen.py

--- screen.py
+++ screen.py
@@ -328,7 +328,7 @@ def main():
             last_tags_and_ids["last_order_tap"] = format_utc_to_est(reader_dict.get("last_order_tap", ""))
             last_tags_and_ids["last_employee_tap"] = format_utc_to_est(reader_dict.get("last_employee_tap", ""))
             last_tags_and_ids["order_id"] = reader_dict.get("order_id", "")
-            last_tags_and_ids["employee_name"] = reader_dict.get("employee_name", "")
+            last_tags_and_ids["employee_name"] = reader_dict.get("employee_name", " ")[0]
     print(last_tags_and_ids)
     # Load the batched button presses from file
     button_presses = load_batch_from_file()
