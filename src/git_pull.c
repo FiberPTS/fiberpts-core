82f177ccd88dc2cbfe3a87d243552407a28e1420
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-09-25 15:06:39 -0400

    Modified file structure
    Modifications made to the file structure.
    
    **Restructured Folders:**
    
    `tests/`: Test files for source files.
    `tests/ec2`: Test files for EC2 API processor source files.
    `tests/iot`: Test files for IoT data collector source files.
    `tests/utility`: Test files for utility source files.
    
    `scripts/build`: Scripts for building.
    `scripts/build/ec2`: Scripts specific to building the EC2 API processor.
    `scripts/build/iot`: Scripts specific to building the IoT data collector.
    `scripts/services`: Service scripts.
    `scripts/services/ec2`: Service scripts for EC2 API processor.
    `scripts/services/iot`: Service scripts for IoT data collector.
    `scripts/utility`: Utility scripts.
    
    `src/ec2`: Source files for EC2 API processor.
    `src/iot`: Source files for IoT data collector.
    `src/utility`: Utility source files.
    
    **Renamed Files:**
    
    `monitor.sh`->`watchdog.sh`: Monitors and resets core program files for IoT data collector
    `configure.sh`->`build.sh`: Build file for IoT data collector
    `button_listener.c`->`operation_tap_listener.c`: Listener for operation taps from touch sensor hardware module.
    `read_ultralight.c`->`nfc_tap_listener.c`: Listener for NFC taps from NFC reader hardware module.
    `screen.py`->`display_and_data_handler.py`: Displays data on the screen hardware module and pushes data to AWS dynamodb.
    `processAPI.py`->`process_api_requests.py`: Processes API requests from AWS dynamodb to Airtable.

--- src/git_pull.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    system("sh /home/potato/NFC_Tracking/pull.sh");
-    system("sh /home/potato/NFC_Tracking/compile_all.sh");
-    system("sh /home/potato/NFC_Tracking/update_permissions.sh");
-    return 0;
-}
