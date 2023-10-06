ac5924a4131ffc25a7b7180bde3d7aa723ed1828
Juan Becerra <jdb9056@nyu.edu>
2023-10-05 22:50:49 -0400

    FPTS-10: Implement parent class, use inheritance

--- src/iot/dynamodb_controller.py
+++ src/iot/dynamodb_controller.py
@@ -1,20 +1,21 @@
+import boto3
+import configparser
+import json
+import time
 from utility.utils import *
 from utility.file_utils import *
 from utility.log_utils import *
 from utility.dynamodb_utils import *
-import boto3
-import json
-import configparser
 
 
 def initialize_dynamodb(table_name):
     """
     Initializes a DynamoDB resource using AWS credentials and returns
     a table object.
-    
+
     Args:
       table_name (str): The name of the DynamoDB table to be initialized.
-    
+
     Returns:
       A DynamoDB table object.
     """
@@ -35,12 +36,142 @@ def initialize_dynamodb(table_name):
     return table
 
 
-class DynamoDBTapHandler:
+class DynamoDB:
 
     def __init__(self, table_name, file_path_info, batch_size):
         self.table = initialize_dynamodb(table_name)
         self.file_path_info = file_path_info
         self.batch_size = batch_size
+
+    def push_item_db(self, table, request_type, request_data):
+        """
+        Pushes an item to a DynamoDB table.
+
+        Args:
+            table (boto3.DynamoDB.Table): The boto3 DynamoDB table resource.
+            request_type: The type of request.
+            request_data: The data for the request.
+
+        Returns:
+            tuple: A tuple containing:
+              - bool: True to indicate successful push, False otherwise.
+              - str: DynamoDB's partition key.
+        """
+        HTTP_OK = 200
+
+        partition_key = f'{get_machine_id()}-{request_type}-{get_current_time()}'
+        response = table.put_item(
+            Item={
+                'partitionKey': partition_key,
+                'Request_Type': request_type,
+                'Data': json.dumps(request_data),
+                'Status': 'Pending',
+            }
+        )
+
+        meta_data = response.get('ResponseMetadata', 'None')
+        if meta_data != 'None':
+            status = meta_data.get('HTTPStatusCode', 'None')
+            if status == HTTP_OK:
+                return True, partition_key
+
+        perror_log(
+            f"An error occurred while pushing the item: \n Data: {request_data} \n MetaData: {meta_data}")
+        return False, None
+
+    def pull_item_db(self, table, partition_key, max_attempts=5):
+        """
+        Pulls an item from a DynamoDB table based on the partition key.
+
+        Args:
+            table (boto3.DynamoDB.Table): The boto3 DynamoDB table resource.
+            partition_key (str): The partition key for the item.
+            max_attempts (int): The maximum number of attempts to retrieve the item.
+
+        Returns:
+            tuple: A tuple containing:
+              - bool: True if successful pull, False otherwise.
+              - str: Item data or an error message.
+        """
+        attempts = 1
+        while attempts <= max_attempts:
+            try:
+                # Retrieve the item from the DynamoDB table
+                response = table.get_item(
+                    Key={
+                        'partitionKey': partition_key
+                    }
+                )
+
+                # Extract the item from the response
+                item = response.get('Item', {})
+
+                # Check if the item exists and has the status "Complete"
+                if item and item.get('Status') == 'Complete':
+                    key = {
+                        'partitionKey': partition_key
+                    }
+                    table.delete_item(Key=key)
+                    return True, item
+                
+                attempts += 1
+                time.sleep(0.1)
+            except Exception as e:
+                perror_log(f"An error occurred while pulling the item: {e}")
+                return False, f"An error occurred: {e}"
+
+        return False, "Item not found or not complete"
+
+    def get_record(self, table, request_data):
+        """
+        Fetch a record from the AWS DynamoDB based on the provided request data.
+
+        Args:
+            table: The boto3 DynamoDB table resource.
+            request_data (dict): The data required to make the request.
+
+        Returns:
+            tuple: (bool, dict or None)
+              - bool: True if data was successfully retrieved, False otherwise.
+              - dict: The record data retrieved from the database, or None if retrieval was unsuccessful.
+        """
+        success, partition_key = self.push_item_db(table, "GetRecord", request_data)
+
+        if success:
+            success, db_data = self.pull_item_db(table, partition_key)
+            if success and 'Data' in db_data:
+                json_data = json.loads(db_data['Data'])['Records'][0]
+                return True, json_data
+
+        return False, None
+
+    def create_record(self, table, request_data):
+        """
+        Create a record to be pushed to the Airtable DB based on the provided request data.
+
+        Args:
+            table: The boto3 DynamoDB table resource.
+            request_data (dict): The data required to make the request.
+
+        Returns:
+            tuple: (bool, dict or None)
+              - bool: True if data was successfully retrieved, False otherwise.
+              - dict: The record data retrieved from the database, or None if retrieval was unsuccessful.
+        """
+        success, partition_key = self.push_item_db(
+            table, "CreateRecord", request_data)
+
+        # TODO: Return record id of the created record.
+        if success:
+            success, db_data = self.pull_item_db(table, partition_key)
+            if success and 'Data' in db_data:
+                json_data = json.loads(db_data['Data'])['Records'][0]
+                return True, json_data
+
+        return False, None
+
+
+class DynamoDBTapHandler(DynamoDB):
 
     def pull_latest_tags_and_ids(self):
         """
@@ -51,10 +182,11 @@ class DynamoDBTapHandler:
 
         Returns:
             tuple: A tuple containing:
-                - bool: True if the data was successfully retrieved and updated, False otherwise.
-                - dict: The updated `last_tags_and_ids` dictionary.
+              - bool: True if the data was successfully retrieved and updated, False otherwise.
+              - dict: The updated `last_tags_and_ids` dictionary.
         """
-        last_tags_and_ids = load_json_from_file(self.file_path_info.LAST_TAGS_AND_IDS)
+        last_tags_and_ids = load_json_from_file(
+            self.file_path_info.LAST_TAGS_AND_IDS)
         machine_id = get_machine_id()
 
         if not last_tags_and_ids.get("machine_record_id"):
@@ -74,10 +206,12 @@ class DynamoDBTapHandler:
                 ]
             }
 
-            is_push_success, partition_key = push_item_db(self.table, "GetRecord", request_data)
+            is_push_success, partition_key = self.push_item_db(
+                self.table, "GetRecord", request_data)
 
             if is_push_success:
-                is_pull_success, data = pull_item_db(self.table, partition_key)
+                is_pull_success, data = self.pull_item_db(
+                    self.table, partition_key)
 
                 if is_pull_success:
                     # Convert data to JSON and extract relevant details
@@ -117,7 +251,7 @@ class DynamoDBTapHandler:
         }
 
         # Attempt to push data to DynamoDB
-        if push_item_db(self.table, "LocalIPAddress", request_data)[0]:
+        if self.push_item_db(self.table, "LocalIPAddress", request_data)[0]:
             print_log(f"Local IP Address pushed successfully: {local_ip}")
             return True
         return False
@@ -135,9 +269,9 @@ class DynamoDBTapHandler:
 
         Returns:
             tuple: A tuple containing three elements:
-                - bool: True if the button tap was handled successfully, False otherwise.
-                - dict: The updated `operation_taps` dictionary.
-                - int: The updated `current_batch_count`.
+              - bool: True if the button tap was handled successfully, False otherwise.
+              - dict: The updated `operation_taps` dictionary.
+              - int: The updated `current_batch_count`.
         """
         if tags_and_ids["last_employee_tag"] != "None" and tags_and_ids["last_order_tag"] != "None":
             print_log("Button Pressed")
@@ -147,12 +281,12 @@ class DynamoDBTapHandler:
                 "order_tag_record_id": tags_and_ids["last_order_record_id"],
                 "timestamp": timestamp
             }
-            
+
             operation_taps["Records"].append(request_data)
             current_batch_count += 1
-            
+
             if current_batch_count >= self.batch_size:
-                if not push_item_db(self.table, "OperationTapEvent", operation_taps):
+                if not self.push_item_db(self.table, "OperationTapEvent", operation_taps):
                     return False, operation_taps, current_batch_count
                 current_batch_count = 0
                 operation_taps = {"Records": []}
@@ -192,7 +326,8 @@ class DynamoDBTapHandler:
             ]
         }
 
-        pull_success, employee_record = get_record(self.table, request_data)
+        pull_success, employee_record = self.get_record(
+            self.table, request_data)
 
         if not employee_record:
             # TODO: Correct the request data format for creating a record
@@ -205,7 +340,7 @@ class DynamoDBTapHandler:
                 }
             }
             last_tags_and_ids["employee_name"] = "NO NAME"
-            employee_record = create_record(self.table, request_data)
+            employee_record = self.create_record(self.table, request_data)
             last_tags_and_ids["last_employee_record_id"] = employee_record["records"][0]["fields"]["Record ID"]
         else:
             # TODO: Confirm format of timestamp
@@ -223,7 +358,8 @@ class DynamoDBTapHandler:
             "employee_tag_record_id": [last_tags_and_ids["last_employee_record_id"]]
         }
 
-        push_success = push_item_db(self.table, "EmployeeNFC", request_data)
+        push_success = self.push_item_db(
+            self.table, "EmployeeNFC", request_data)
 
         if not push_success:
             last_tags_and_ids.update({
@@ -264,7 +400,7 @@ class DynamoDBTapHandler:
             ]
         }
 
-        pull_success, order_record = get_record(self.table, request_data)
+        pull_success, order_record = self.get_record(self.table, request_data)
 
         if not order_record:
             return self.handle_employee_tap(self.table, last_tags_and_ids, tag_uid, timestamp)
@@ -287,7 +423,7 @@ class DynamoDBTapHandler:
             "employee_tag_record_id": [last_tags_and_ids["last_employee_record_id"]]
         }
 
-        if not push_item_db(self.table, "OrderNFC", request_data):
+        if not self.push_item_db(self.table, "OrderNFC", request_data):
             return False
 
         return True
