453b1cddecc9411ffdf63d2997d986646d71f622
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2023-10-08 16:38:28 -0400

    Adding debug statements to watchdog.sh

--- scripts/services/iot/watchdog.sh
+++ scripts/services/iot/watchdog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Script started."
+
 # Define the base directory for the NFC tracking application.
 BASE_DIR="/home/potato/FiberPTS/src/iot"
 
@@ -10,12 +12,15 @@ PROGRAM_NAMES=(
     "tap_event_handler.py"
 )
 
+echo "Defining program paths."
 for idx in "${!PROGRAM_NAMES[@]}"; do
-    PROGRAM_PATHS[$idx]="{$BASE_DIR}/${PROGRAM_NAMES[$idx]}"
+    PROGRAM_PATHS[$idx]="${BASE_DIR}/${PROGRAM_NAMES[$idx]}"
 done
 
+echo "Setting up SIGTERM handler."
 # Kill the monitored programs when the script receives a SIGTERM signal.
 on_sigterm() {
+    echo "Received SIGTERM. Killing monitored programs."
     kill "$(cat /var/run/nfc_tap_listener.pid)"
     kill "$(cat /var/run/operation_tap_listener.pid)"
     kill "$(cat /var/run/tap_event_handler.pid)"
@@ -27,21 +32,18 @@ trap on_sigterm SIGTERM
 
 # Function to check WiFi connection and reconnect if disconnected
 check_wifi() {
+    echo "Checking WiFi connection."
     # Check if "FERRARAMFG" is in the list of available WiFi networks
     if nmcli device wifi list | grep -q "FERRARAMFG"; then
         # Check if not already connected to "FERRARAMFG"
         if ! nmcli con show --active | grep -q "FERRARAMFG"; then
+            echo "Connecting to FERRARAMFG."
             # Try to connect to "FERRARAMFG"
             nmcli device wifi connect FERRARAMFG password FerraraWIFI1987
             # Ensure connection is up
             nmcli con up FERRARAMFG
         fi
     fi
-    # Uncomment the below lines if you want to also check for "iPhone (202)"
-    #if ! nmcli con show --active | grep -q "iPhone (202)"; then
-    #    nmcli device wifi connect "iPhone (202)" password thehomiepass
-    #    nmcli con up "iPhone (202)"
-    #fi
 }
 
 # Extend the Python path to include a custom directory.
@@ -52,12 +54,13 @@ while true; do
     # Check the WiFi connection.
     check_wifi
 
+    echo "Checking if the programs are running."
     # Check if the first program is running.
-    if pgrep -f "${PROGRAM_NAMES[0]}" > /dev/null
-    then
+    if pgrep -f "${PROGRAM_NAMES[0]}" > /dev/null; then
         STATUS_1="Online"
     else
         STATUS_1="Offline"
+        echo "Starting the first program."
         # If not running, try to restart the program and log its output.
         "${PROGRAM_PATHS[0]}" >> /var/log/programs.log 2>&1 &
         # Store the PID of the restarted program.
@@ -65,11 +68,11 @@ while true; do
     fi
 
     # Check if the second program is running.
-    if pgrep -f "${PROGRAM_NAMES[1]}" > /dev/null
-    then
+    if pgrep -f "${PROGRAM_NAMES[1]}" > /dev/null; then
         STATUS_2="Online"
     else
         STATUS_2="Offline"
+        echo "Starting the second program."
         # If not running, try to restart the program and log its output.
         "${PROGRAM_PATHS[1]}" >> /var/log/programs.log 2>&1 &
         # Store the PID of the restarted program.
@@ -77,25 +80,26 @@ while true; do
     fi
 
     # Check if the third program is running.
-    if pgrep -f "${PROGRAM_PATHS[2]}" > /dev/null
-    then
+    if pgrep -f "${PROGRAM_PATHS[2]}" > /dev/null; then
         STATUS_3="Online"
     else
         STATUS_3="Offline"
+        echo "Starting the third program."
         # If not running, try to restart the program and log its output.
         "${PROGRAM_PATHS[2]}" >> /var/log/programs.log 2>&1 &
         # Store the PID of the restarted program.
         echo $! > /var/run/tap_event_handler.pid
     fi
 
+    echo "Checking overall status."
     # Determine the overall status of all programs.
-    if [ "$STATUS_1" = "Offline" ] || [ "$STATUS_2" = "Offline" ] || [ "$STATUS_3" = "Offline" ]
-    then
+    if [ "$STATUS_1" = "Offline" ] || [ "$STATUS_2" = "Offline" ] || [ "$STATUS_3" = "Offline" ]; then
         STATUS="Offline"
     else
         STATUS="Online"
     fi
 
-    # Pause for 60 seconds before checking the programs again.
+    echo "Sleeping for 10 seconds."
+    # Pause for 10 seconds before checking the programs again.
     sleep 10
 done
