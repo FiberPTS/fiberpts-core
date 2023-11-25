53ff83019b22ac70b5b41d3ebcbb7c28e64f5705
Juan Becerra <jdb9056@nyu.edu>
2023-11-24 20:08:19 -0500

    Refactor entire codebase

--- scripts/utility/compile_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd $HOME/NFC_Tracking
-for file in *.c; do
-    output="${file%.*}"
-    gcc -o "$output" "$file" -l nfc -l curl -l json-c -l gpiod
-done
--- src/iot/utility/log_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdarg.h>
-#include <stdint.h>
-#include <stddef.h>
-// TODO: Find and remove libraries that are unnecessary
-
-void print_log(const char *program_name, const char *format, ...);
-void perror_log(const char *program_name, const char *format, ...);
-void print_hex(const char *program_name, const uint8_t *pbtData, const size_t szBytes);
