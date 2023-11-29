2c2de158b76738b84de76b2f9472b4ff7ff62b98
Juan Becerra <jdb9056@nyu.edu>
2023-11-29 00:09:57 -0500

    Commit sending single tap to screen pipe test

--- tests/touch_sensor/test_touch_sensor.py
+++ tests/touch_sensor/test_touch_sensor.py
@@ -1,13 +1,20 @@
+import tempfile
 import time
+from unittest.mock import patch, mock_open
 
+import json
 import pytest
+
+from src.touch_sensor.touch_sensor import TouchSensor
 
 DEBOUNCE_TIME = 0.001
 
 
 @pytest.fixture
 def touch_sensor():
-    return TouchSensor(debounce=DEBOUNCE_TIME)
+    with tempfile.NamedTemporaryFile() as tmp_fifo:
+        fifo_path = tmp_fifo.name
+        yield TouchSensor(debounce=DEBOUNCE_TIME, screen_pipe_path=fifo_path)
 
 
 class TestDebounceBehavior:
@@ -18,18 +25,43 @@ class TestDebounceBehavior:
         (DEBOUNCE_TIME, True), 
         (DEBOUNCE_TIME + 0.0001, True)
     ])
-    def test_values(self, request: pytest.FixtureRequest):
+    def test_values(self, request):
         return request.param
 
-    def test_basic_debounce_behavior(self, touch_sensor: Any, test_values: tuple):
+    def test_basic_debounce_behavior(self, touch_sensor, test_values):
         wait_time, expected = test_values
         touch_sensor.tap()
         time.sleep(wait_time)
         assert touch_sensor.tap() == expected
 
-    def test_reset_debounce_time_behavior(self, touch_sensor: Any, test_values: tuple):
+    def test_reset_debounce_time_behavior(self, touch_sensor, test_values):
         wait_time, expected = test_values
         touch_sensor.tap()
         touch_sensor.tap()  # Reset debounce time
         time.sleep(wait_time)
         assert touch_sensor.tap() == expected
+
+
+class TestSendingRequestToScreenPipe:  
+
+    @pytest.fixture
+    def mock_fifo(self):
+        return mock_open()  # Simulate file object
+
+    @pytest.parametrize('type', [('success'), ('failure')])
+    def test_sending_single_request_to_screen_pipe(self, 
+                                                   type, 
+                                                   mock_fifo, 
+                                                   touch_sensor
+    ):
+        sample_data = {
+            'type': type, 
+            'data': {
+                'timestamp': time.srtftime('%Y-%m-%dT%X', time.gmtime(0))
+            }
+        }
+        
+        # Ensures that when TouchSensor calls open, it uses the mocked version
+        with patch('src.touch_sensor.touch_sensor.open', mock_fifo):
+            touch_sensor.send_to_screen(type, time.gmtime(0))
+            mock_fifo().write.assert_called_once_with(sample_data)
