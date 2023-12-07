fde7a29cd2f6c6adbe38ffbb93faaf84ea0a89b1
Juan Becerra <jdb9056@nyu.edu>
2023-12-07 18:48:09 -0500

    Commit function to get Unix-based device

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,9 +1,27 @@
 import subprocess
 import time
+import os
 
-DEVICE_ID = subprocess.check_output('cat /etc/machine_id', shell=True)
 
 TIMESTAMP_FORMAT = '%Y-%m-%d %X'
+
+
+def __get_device_id() -> str:
+    """Retrieve the device ID from a Unix-like file system.
+
+    This function is implemented to avoid errors when referencing Unix-like file
+    structure on Windows computers.
+
+    Returns:
+        str: The device ID if the file exists, otherwise an empty string.
+    """
+    device_id = ''
+    if os.path.exists('/etc/machine-id'):
+        device_id = subprocess.check_output('cat /etc/machine-id', shell=True)
+    return device_id
+
+
+DEVICE_ID = __get_device_id()
 
 
 def ftimestamp(timestamp: float) -> str:
