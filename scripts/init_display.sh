b33290abeb4a48fe75af7db175c282dbe57677a1
NxFerrara <nxferrara1981@gmail.com>
2023-12-07 23:45:58 -0500

    Add display initialization scripts

---
+++ scripts/init_display.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Map the console to the framebuffer
+con2fbmap 1 2
+# Wait for a moment
+sleep 0.5
+# Unmap the console
+con2fbmap 1 0
+# Clear the screen
+dd if=/dev/zero of=/dev/fb2 bs=1 count=153600
