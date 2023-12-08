1af7691de06f75adfc97b0212720d8414eb348f7
Juan Becerra <jdb9056@nyu.edu>
2023-12-08 01:11:03 -0500

    Commit cloud database client class
    Provides specific function for communicating with Supabase. Reequires error handling for next sprint.

--- src/cloud_db/cloud_db.py
+++ src/cloud_db/cloud_db.py
@@ -1,18 +1,43 @@
 import os
 
 from dotenv import load_dotenv
-from supabase import create_client, APIResponse
+import supabase
+from postgrest import APIResponse
 
+from touch_sensor.touch_sensor import Tap
+from utils.touch_sensor_utils import tap_to_db_entry
 
 load_dotenv()
 
 
 class CloudDBClient:
+    """Client for interacting with a cloud database using the Supabase API."""
 
     def __init__(self):
+        """
+        Initializes the client with database credentials.
+
+        Attributes:
+            client: Supabase Client instance.
+        """
         url = os.getenv('DATABASE_URL')
         key = os.getenv('DATABASE_API_KEY')
-        self.client = create_client(url, key)
+        self.client = supabase.create_client(url, key)
     
-    def insert_tap_data_entry(self, table: str, data: dict) -> ...:
-        pass
+    def insert_tap_data(self, tap: Tap) -> APIResponse:
+        """
+        Inserts tap data into the database.
+
+        Args:
+            tap: A Tap object containing the data to be inserted.
+
+        Returns:
+            An APIResponse object representing the result of the database operation.
+        """
+        # TODO: Implement handling for connection issues.
+        # TODO: Implement handling for authentication issues.
+        # TODO: Implement data validation.
+        # TODO: Implement handling for non-existent table.
+        tap_data = tap_to_db_entry(tap)
+        response = self.client.table('tap_data').insert(tap_data).execute()
+        return response
