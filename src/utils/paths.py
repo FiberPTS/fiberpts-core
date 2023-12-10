fe85792f31dd8093c43123cb9aa69d4256d5fe90
NxFerrara <nxferrara1981@gmail.com>
2023-12-10 18:37:52 -0500

    Create paths.py

---
+++ src/utils/paths.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+
+load_dotenv(f"{script_dir}/.env.shared")
+
+# File paths to named pipes
+TOUCH_SENSOR_TO_SCREEN_PIPE = os.getenv('TOUCH_SENSOR_TO_SCREEN_PIPE')
+NFC_TO_SCREEN_PIPE = os.getenv('NFC_TO_SCREEN_PIPE')
+
+# File path to display framebuffer
+DISPLAY_FRAME_BUFFER_PATH = os.getenv('DISPLAY_FRAME_BUFFER_PATH')
+
+# File path to device state
+DEVICE_STATE_FILE_PATH = os.getenv('DEVICE_STATE_FILE_PATH')
+
+print(TOUCH_SENSOR_TO_SCREEN_PIPE)
