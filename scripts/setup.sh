200f69b9e392daa38dc2087c5de27b92353c6ad3
NxFerrara <nxferrara1981@gmail.com>
2023-12-09 22:58:52 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-# For initial device setup
+# Load the environment variables from the .env.shared file
+set -a  # automatically export all variables
+PROJECT_DIR="/opt"
+PROJECT_PATH="$PROJECT_DIR/FiberPTS"
+source "$PROJECT_DIR/FiberPTS/.env.shared"
+set +a  # stop automatically exporting
