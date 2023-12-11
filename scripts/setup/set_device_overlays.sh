4529fbd2068a8230056052a6fb6027febb730d2a
NxFerrara <nxferrara1981@gmail.com>
2023-12-10 23:39:52 -0500

    Fix command execution from sh to bash

--- scripts/setup/set_device_overlays.sh
+++ scripts/setup/set_device_overlays.sh
@@ -16,7 +16,7 @@ assert_conditions() {
 install_libretech_wiring_tool() {
     if [ ! -d "$PROJECT_DIR/libretech-wiring-tool" ]; then
         git clone https://github.com/libre-computer-project/libretech-wiring-tool.git "$PROJECT_DIR/libretech-wiring-tool" || { echo "Git clone failed"; exit 1; }
-        sh "$PROJECT_DIR/libretech-wiring-tool/install.sh" || { echo "Install script failed"; exit 1; }
+        bash "$PROJECT_DIR/libretech-wiring-tool/install.sh" || { echo "Install script failed"; exit 1; }
         "$PROJECT_DIR/libretech-wiring-tool/ldto" merge uart-a spi-cc-cs1 spi-cc-1cs-ili9341 || { echo "ldto merge command failed"; exit 1; }
     fi
 }
