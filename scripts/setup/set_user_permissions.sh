fc5c7a0fc0d235f57377c5769ae779f603781754
NxFerrara <nxferrara1981@gmail.com>
2023-12-10 19:35:35 -0500

    Update perms & add TODOs

--- scripts/setup/set_user_permissions.sh
+++ scripts/setup/set_user_permissions.sh
@@ -30,6 +30,7 @@ create_and_assign_group() {
 }
 
 set_ownership_and_permissions() {
+    # Set group ownership to relevant files
     chown -R :"$GROUP_NAME" "$PROJECT_PATH"/src || return 1
     chown -R :"$GROUP_NAME" "$PROJECT_PATH"/scripts/run || return 1
     chown -R :"$GROUP_NAME" "$PROJECT_PATH"/config || return 1
@@ -37,13 +38,17 @@ set_ownership_and_permissions() {
     chown :"$GROUP_NAME" "$PROJECT_PATH"/.env || return 1
     chown -R :"$GROUP_NAME" "$PROJECT_PATH"/app || return 1
 
-    find "$PROJECT_PATH"/src -type f -name "*.py" ! -name "__init__.py" -exec chmod 770 {} \; || return 1
+    # Set executable permissions
+    # TODO: This is done after shebang is added to all source files when the scripts can be executed after activating the virtual environment (no absolute path execution)
+    # find "$PROJECT_PATH"/src -type f -name "*.py" ! -name "__init__.py" -exec chmod 770 {} \; || return 1
     find "$PROJECT_PATH"/scripts/run -type f -name "*.sh" -exec chmod 770 {} \; || return 1
 
+    # Set readable permissions
     find "$PROJECT_PATH"/config -type f -name "*.py" ! -name "__init__.py" -exec chmod 440 {} \; || return 1
     chmod 440 "$PROJECT_PATH"/.env.shared || return 1
-    chmod 440 "$PROJECT_PATH"/.env.secure || return 1
+    chmod 440 "$PROJECT_PATH"/.env || return 1
 
+    # Set readable/writable permissions
     chmod -R 660 "$PROJECT_PATH"/app || return 1
 }
 
