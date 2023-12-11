d201b3b9813e9e04e927452342c292a1c9f56be7
NxFerrara <nxferrara1981@gmail.com>
2023-12-11 13:23:08 -0500

    Change /app to executable perms
    To open a file in a directory, a user needs execute permission on the directory. This is true even if the user has read permission on the file itself.

--- scripts/setup/set_user_permissions.sh
+++ scripts/setup/set_user_permissions.sh
@@ -32,10 +32,10 @@ create_and_assign_group() {
         echo "Group $GROUP_NAME already exists. Skipping group creation."
     else
         groupadd "$GROUP_NAME" || return 1
+        usermod -a -G "$GROUP_NAME" "$USERNAME" || return 1
+        # Add to any other necessary groups
+        usermod -a -G video "$USERNAME" || return 1
     fi
-    usermod -a -G "$GROUP_NAME" "$USERNAME" || return 1
-    # Add to any other necessary groups
-    usermod -a -G video "$USERNAME" || return 1
 }
 
 set_ownership_and_permissions() {
@@ -51,14 +51,12 @@ set_ownership_and_permissions() {
     # TODO: This is done after shebang is added to all source files when the scripts can be executed after activating the virtual environment (no absolute path execution)
     # find "$PROJECT_PATH"/src -type f -name "*.py" ! -name "__init__.py" -exec chmod 770 {} \; || return 1
     find "$PROJECT_PATH"/scripts/run -type f -name "*.sh" -exec chmod 770 {} \; || return 1
+    chmod -R 770 "$PROJECT_PATH"/app || return 1
 
     # Set readable permissions
     find "$PROJECT_PATH"/config -type f -name "*.py" ! -name "__init__.py" -exec chmod 440 {} \; || return 1
     chmod 440 "$PROJECT_PATH"/.env.shared || return 1
     chmod 440 "$PROJECT_PATH"/.env || return 1
-
-    # Set readable/writable permissions
-    chmod -R 660 "$PROJECT_PATH"/app || return 1
 
     echo "Group ownership and permissions set"
 }
