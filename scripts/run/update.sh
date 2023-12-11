5b5415085cfd2a72713473c916f8c6cc1ff5e3dc
NxFerrara <nxferrara1981@gmail.com>
2023-12-11 00:50:56 -0500

    Update update.sh

--- scripts/run/update.sh
+++ scripts/run/update.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+assert_conditions() {
+    # Root check
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "This script must be run as root. Please use sudo."
+        exit 1
+    fi
+}
+
 # For updating the device with new repository changes
+load_env_variables() {
+    set -a # Exports all environment variables
+    source "$SCRIPT_DIR/../.env.shared" || return 1
+    source "$SCRIPT_DIR/../.env" || return 1
+    set +a # Stops exporting environment variables
+}
+
+
+assert_conditions
+load_env_variables
+bash "$SCRIPT_DIR/../setup/set_user_permissions.sh"
