3bd13cc785f314cbb180528d004d5ef50d1c3d89
NxFerrara <nxferrara1981@gmail.com>
2023-12-11 01:12:34 -0500

    Fix dynamic framebuffer initialization

--- scripts/run/init.sh
+++ scripts/run/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+env_file="$SCRIPT_DIR/../../.env.shared"
 
-source "$SCRIPT_DIR/../../.env.shared"
+source "$env_file"
 
-# Check if DISPLAY_FRAME_BUFFER_PATH exists
-if [ -e "$DISPLAY_FRAME_BUFFER_PATH" ]; then
+init_display(){
     # Map the console to the framebuffer
     con2fbmap 1 2
     # Wait for a moment
@@ -14,7 +14,22 @@ if [ -e "$DISPLAY_FRAME_BUFFER_PATH" ]; then
     con2fbmap 1 0
     # Clear the screen
     sudo dd if=/dev/zero of="$DISPLAY_FRAME_BUFFER_PATH" bs=1 count=153600
+}
+
+# Check if DISPLAY_FRAME_BUFFER_PATH exists
+if [ -e "$DISPLAY_FRAME_BUFFER_PATH" ]; then
+    init_display
 else
-    echo "Error: DISPLAY_FRAME_BUFFER_PATH does not exist."
-    exit 1
+    echo "WARNING: $DISPLAY_FRAME_BUFFER_PATH does not exist."
+    if [ -e "$DISPLAY_FRAME_BUFFER_PATH_ALT" ]; then
+        # Swap the DISPLAY_FRAME_BUFFER_PATH and DISPLAY_FRAME_BUFFER_PATH_ALT using sed
+        sed -i "s|^DISPLAY_FRAME_BUFFER_PATH=.*|DISPLAY_FRAME_BUFFER_PATH=${DISPLAY_FRAME_BUFFER_PATH_ALT}|" "$env_file"
+        sed -i "s|^DISPLAY_FRAME_BUFFER_PATH_ALT=.*|DISPLAY_FRAME_BUFFER_PATH_ALT=${DISPLAY_FRAME_BUFFER_PATH}|" "$env_file"
+
+        source "$env_file"
+        init_display
+    else
+        echo "Error: $DISPLAY_FRAME_BUFFER_PATH_ALT does not exist."
+        exit 1
+    fi
 fi
