a7194af6ca585fc7b053ac5dcff2a696b93688e6
NxFerrara <nxferrara1981@gmail.com>
2023-12-10 19:37:44 -0500

    Remove daemon-reload

--- scripts/setup/create_services.sh
+++ scripts/setup/create_services.sh
@@ -15,29 +15,18 @@ assert_conditions() {
 
 process_service_files() {
     local service_dir="$PROJECT_PATH/services"
-    local service_filenames=()
 
     export MAINPID=''
 
     for service_template in "$service_dir"/*.service; do
-        if [ -f "$service_template" ]; then
-            local service_filename=$(basename "$service_template")
-            envsubst < "$service_template" > "$SYSTEMD_DIR/$service_filename"
-            service_filenames+=("$service_filename")
-            echo "Service file created: $service_filename"
-        else
-            echo "No service templates found in $service_dir"
-        fi
-    done
-
-    systemctl daemon-reload
-
-    for service_filename in "${service_filenames[@]}"; do
+        local service_filename=$(basename "$service_template")
+        envsubst < "$service_template" > "$SYSTEMD_DIR/$service_filename"
+        echo "Service file created: $service_filename"
         systemctl enable "$service_filename"
-        echo "Service enabled: $service_filename"
+        echo "Service file enabled: $service_filename"
     done
 
-    echo "All service files are now processed, daemon reloaded, and services enabled."
+    echo "All service files are now processed and services enabled."
 }
 
 main() {
