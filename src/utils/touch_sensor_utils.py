392f23d16038b330340f656cdeea0e0fae442a7d
Juan Becerra <jdb9056@nyu.edu>
2023-12-10 20:26:11 -0500

    Change name

--- src/utils/touch_sensor_utils.py
+++ src/utils/touch_sensor_utils.py
@@ -8,7 +8,7 @@ class Tap(NamedTuple):
     """Stores information describing a tap event.
 
     Attributes:
-        device_id: The ID of the device from which the tap was made.
+        machine_id: The ID of the machine from which the tap was made.
         timestamp: The timestamp at which the tap was made.
         status: A TapStatus indicating the status of the tap.
     """
