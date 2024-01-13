c7f5f815e66349d8bdcf3618b9e43e9161b39f0b
NxFerrara <nxferrara1981@gmail.com>
2024-01-13 13:17:31 -0500

    Fix local reference

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -88,7 +88,7 @@ main() {
     assert_root
     parse_arguments "$@"
 
-    local pre_reboot_flag_file_path="$PROJECT_PATH/app/tmp/pre_reboot_installed"
+    pre_reboot_flag_file_path="$PROJECT_PATH/app/tmp/pre_reboot_installed"
     local pre_reboot_scripts=("create_venv.sh" "install_dependencies.sh" "set_device_overlays.sh" "install_wifi_driver.sh" "create_pipes.sh" "set_user_permissions.sh" "create_services.sh")
     local post_reboot_scripts=("set_device_overlays.sh" "connect_wifi.sh")
 
