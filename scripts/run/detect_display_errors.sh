d074bd76a83523867dc8c220097f3d5b126d73cb
Juan Becerra <jdb9056@nyu.edu>
2024-01-24 16:01:16 -0500

    Replace magic value with global

--- scripts/run/detect_display_errors.sh
+++ scripts/run/detect_display_errors.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DISPLAY_ERROR_MESSAGE="panel-ilitek-ili9341.*SPI transfer failed|spi_master.*failed to transfer one message from queue|panel-ilitek-ili9341.*\*ERROR\* Failed to update display"
 
 env_file="$SCRIPT_DIR/../../app/.env.shared"
 
@@ -15,20 +16,20 @@ error_count_threshold=$DISPLAY_ERROR_COUNT_THRESHOLD # Replace with your actual 
 error_count=0
 
 # Extract the timestamp of the most recent error message
-last_error_timestamp=$(dmesg | grep -E 'panel-ilitek-ili9341.*SPI transfer failed|spi_master.*failed to transfer one message from queue|panel-ilitek-ili9341.*\*ERROR\* Failed to update display' | tail -1 | grep -oP '\[\s*\K\d+\.\d+')
+last_error_timestamp=$(dmesg | grep -E "$DISPLAY_ERROR_MESSAGE" | tail -1 | grep -oP '\[\s*\K\d+\.\d+')
 
 # Initialize the current timestamp and counter
 if [ -z "$last_error_timestamp" ]; then
     last_error_timestamp=0
 else
-    initial_errors=$(dmesg | grep -c -E 'panel-ilitek-ili9341.*SPI transfer failed|spi_master.*failed to transfer one message from queue|panel-ilitek-ili9341.*\*ERROR\* Failed to update display')
+    initial_errors=$(dmesg | grep -c -E "$DISPLAY_ERROR_MESSAGE")
     error_count=$((initial_errors % error_count_threshold))
 fi
 
 # Start monitoring dmesg
 dmesg --follow | while read line; do
     # Check if the message is an error message
-    if echo "$line" | grep -q -E 'panel-ilitek-ili9341.*SPI transfer failed|spi_master.*failed to transfer one message from queue|panel-ilitek-ili9341.*\*ERROR\* Failed to update display'; then
+    if echo "$line" | grep -q -E "$DISPLAY_ERROR_MESSAGE"; then
         # Extract the floating-point timestamp
         new_error_timestamp=$(echo "$line" | grep -oP '\[\s*\K\d+\.\d+')
         # Update the last timestamp and reset counter if the error is new
