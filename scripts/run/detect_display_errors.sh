3497b441d5b5dd3d5ec18bd2fc647ecbd2b6c6c7
Juan Becerra <jdb9056@nyu.edu>
2024-01-24 16:18:31 -0500

    Fix format and code organization
    Removed unnecessary commenting and dummy variables. Add short description to script.

--- scripts/run/detect_display_errors.sh
+++ scripts/run/detect_display_errors.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
+# Monitors system logs for specific display driver errors (panel-ilitek-ili9341
+# and spi_master) and resets the display drivers when a certain number of these
+# errors occur within a predefined time frame. Reset operation is performed
+# safely by using a file-descriptor-based locking mechanism.
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DISPLAY_ERROR_MESSAGE="panel-ilitek-ili9341.*SPI transfer failed|spi_master.*failed to transfer one message from queue|panel-ilitek-ili9341.*\*ERROR\* Failed to update display"
 
-env_file="$SCRIPT_DIR/../../app/.env.shared"
+source "$SCRIPT_DIR/../../app/.env.shared"
+exec 200> "$DISPLAY_FRAME_BUFFER_LOCK_PATH"
 
-source "$env_file"
+# Unlock file descriptor 200 on script exit
+trap 'flock -u 200' EXIT
 
-exec 200>"$DISPLAY_FRAME_BUFFER_LOCK_PATH" # Open the lock file for writing and assign file descriptor 200
-
-trap 'flock -u 200' EXIT # Trap EXIT signal to ensure lock release on script exit
-
-# Define the max_new_errors threshold
-error_count_threshold=$DISPLAY_ERROR_COUNT_THRESHOLD # Replace with your actual threshold
 error_count=0
-
-# Extract the timestamp of the most recent error message
 last_error_timestamp=$(dmesg | grep -E "$DISPLAY_ERROR_MESSAGE" | tail -1 | grep -oP '\[\s*\K\d+\.\d+')
 
-# Initialize the current timestamp and counter
 if [ -z "$last_error_timestamp" ]; then
     last_error_timestamp=0
 else
@@ -26,23 +24,18 @@ else
     error_count=$((initial_errors % error_count_threshold))
 fi
 
-# Start monitoring dmesg
 dmesg --follow | while read line; do
-    # Check if the message is an error message
     if echo "$line" | grep -q -E "$DISPLAY_ERROR_MESSAGE"; then
-        # Extract the floating-point timestamp
         new_error_timestamp=$(echo "$line" | grep -oP '\[\s*\K\d+\.\d+')
-        # Update the last timestamp and reset counter if the error is new
         if (( $(echo "$new_error_timestamp > $last_error_timestamp" | bc -l) )); then
-            # Error counting logic
             ((error_count++))
-            if [ "$error_count" -gt "$error_count_threshold" ]; then
-                flock -x 200 # Wait until lock is acquired
-                rmmod ili9341
+            if [ "$error_count" -gt "$DISPLAY_ERROR_COUNT_THRESHOLD" ]; then
+                flock -x 200
+                rmmod ili9341     # Unload ili9341 driver
                 sleep 0.5
-                modprobe ili9341
+                modprobe ili9341  # Reload ili9341 driver
                 sleep 0.5
-                flock -u 200 # Release the lock
+                flock -u 200
                 error_count=0
             fi
         fi
