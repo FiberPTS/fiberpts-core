1b79304808ce25ed1e22c5ef4fdc8bc160293cf2
NxFerrara <nxferrara1981@gmail.com>
2024-02-01 11:04:49 -0500

    Fix formatting for create_pipes.sh

--- scripts/setup/pre-reboot/create_pipes.sh
+++ scripts/setup/pre-reboot/create_pipes.sh
@@ -19,17 +19,17 @@ create_fifo_pipes() {
     echo -e "\nCreating FIFO pipes..."
 
     if [ -p "$TOUCH_SENSOR_TO_SCREEN_PIPE" ]; then
-        echo "\033[0;33m[WARNING]\033\0m\tFIFO pipe '$TOUCH_SENSOR_TO_SCREEN_PIPE' already exists."
+        echo -e "\033[0;33m[WARNING]\033[0m\tFIFO pipe '$TOUCH_SENSOR_TO_SCREEN_PIPE' already exists."
     else
         mkfifo "$TOUCH_SENSOR_TO_SCREEN_PIPE"
-        echo "\033[0;32m[OK]\033[0m\t\tFIFO pipe '$TOUCH_SENSOR_TO_SCREEN_PIPE' created."
+        echo -e "\033[0;32m[OK]\033[0m\t\tFIFO pipe '$TOUCH_SENSOR_TO_SCREEN_PIPE' created."
     fi
 
     if [ -p "$NFC_TO_SCREEN_PIPE" ]; then
-        echo "\033[0;33m[WARNING]\033\0m\tFIFO pipe '$NFC_TO_SCREEN_PIPE' already exists."
+        echo -e "\033[0;33m[WARNING]\033[0m\tFIFO pipe '$NFC_TO_SCREEN_PIPE' already exists."
     else
         mkfifo "$NFC_TO_SCREEN_PIPE"
-        echo "\033[0;32m[OK]\033[0m\t\tFIFO pipe '$NFC_TO_SCREEN_PIPE' created."
+        echo -e "\033[0;32m[OK]\033[0m\t\tFIFO pipe '$NFC_TO_SCREEN_PIPE' created."
     fi
 }
 
