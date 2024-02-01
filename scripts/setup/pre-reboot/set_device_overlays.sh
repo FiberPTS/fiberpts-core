b1a6176b816dc1c45564b4dd77d082b3402eded9
NxFerrara <nxferrara1981@gmail.com>
2024-02-01 12:35:18 -0500

    Update merge logic overlays + Add user input

--- scripts/setup/pre-reboot/set_device_overlays.sh
+++ scripts/setup/pre-reboot/set_device_overlays.sh
@@ -7,8 +7,8 @@ assert_conditions() {
         exit 1
     fi
 
-    if [ -z "$PROJECT_DIR" ] || [ -z "$PROJECT_PATH" ]; then
-        echo "Required environment variable PROJECT_DIR is not set."
+    if [ -z "$PROJECT_DIR" ] || [ -z "$PROJECT_PATH" ] || [ -z "$OVERLAY_MERGED_FLAG_FILE" ]; then
+        echo "Required environment variables PROJECT_DIR, PROJECT_PATH, and OVERLAY_MERGED_FLAG_FILE is not set."
         exit 1
     fi
 }
@@ -18,50 +18,55 @@ set_custom_dts() {
 }
 
 install_libretech_wiring_tool() {
-    local pre_reboot_flag_file_path=$1
-    local overlay_merged_flag_file_path=$2
-
-    if [ ! -f "$pre_reboot_flag_file_path" ] ; then
-        if [ ! -d "$PROJECT_DIR/libretech-wiring-tool" ]; then
-            echo "Installing libretech-wiring-tool..."
-            git clone https://github.com/libre-computer-project/libretech-wiring-tool.git "$PROJECT_DIR/libretech-wiring-tool" || { echo "Git clone failed"; exit 1; }
-            set_custom_dts
-            bash "$PROJECT_DIR/libretech-wiring-tool/install.sh" || { echo "Install script failed"; exit 1; }
-        else
-            echo "Already installed libretech-wiring-tool..."
-            if [ -f "$overlay_merged_flag_file_path" ]; then
-                echo "Resetting overlays..."
-                rm -f "$overlay_merged_flag_file_path"
-                /opt/libretech-wiring-tool/ldto reset
-                echo "Overlays reset. Reboot to apply changes."
-            fi
-        fi
+    if [ ! -d "$PROJECT_DIR/libretech-wiring-tool" ]; then
+        echo "Installing libretech-wiring-tool..."
+        git clone https://github.com/libre-computer-project/libretech-wiring-tool.git "$PROJECT_DIR/libretech-wiring-tool" || { echo "Git clone failed"; exit 1; }
+        set_custom_dts
+        bash "$PROJECT_DIR/libretech-wiring-tool/install.sh" || { echo "Install script failed"; exit 1; }
+        echo "Installed libretech-wiring-tool."
     else
-        if [ -d "$PROJECT_DIR/libretech-wiring-tool" ]; then
-            echo "Already installed libretech-wiring-tool..."
-            if [ -f "$overlay_merged_flag_file_path" ]; then
+        echo "Already installed libretech-wiring-tool..."
+    fi
+    if [ -f "$OVERLAY_MERGED_FLAG_FILE" ]; then
+        local reset_answer=-1
+        while [ reset_answer -ne 0 ] && [ reset_answer -ne 1 ]; do
+            echo "Do you want to reset the overlays?"
+            echo -n "Yes (1) or No (0): "
+            read reset_answer
+            if [ "$reset_answer" -eq 1 ]; then
                 echo "Resetting overlays..."
-                rm -f "$overlay_merged_flag_file_path"
+                rm -f "$OVERLAY_MERGED_FLAG_FILE"
                 /opt/libretech-wiring-tool/ldto reset
                 echo "Overlays reset. Reboot to apply changes."
+            elif [ "$reset_answer" -eq 0 ]; then
+                echo "No action taken."
             else
+                echo "Invalid input...Please try again."
+            fi
+        done
+    else 
+        local merge_answer=-1
+        while [ merge_answer -ne 0 ] && [ merge_answer -ne 1 ]; do
+            echo "Do you want to merge the overlays?"
+            echo -n "Yes (1) or No (0): "
+            read merge_answer
+            if [ "$merge_answer" -eq 1 ]; then
                 echo "Merging overlays..."
-                touch "$overlay_merged_flag_file_path"
+                touch "$OVERLAY_MERGED_FLAG_FILE"
                 /opt/libretech-wiring-tool/ldto merge uart-a spi-cc-cs1 spi-cc-1cs-ili9341 || { echo "ldto merge command failed"; exit 1; }
                 echo "Overlays merged. Reboot to apply changes."
+            elif [ "$merge_answer" -eq 0 ]; then
+                echo "No action taken."
+            else
+                echo "Invalid input...Please try again."
             fi
-        else
-            echo "Failed installation during pre-reboot phase."
-        fi
+        done
     fi
 }
 
 main() {
-    local pre_reboot_flag_file_path="$PROJECT_PATH/app/tmp/pre_reboot_installed"
-    local overlay_merged_flag_file_path="$PROJECT_PATH/app/tmp/overlay_merged"
-
     assert_conditions
-    install_libretech_wiring_tool "$pre_reboot_flag_file_path" "$overlay_merged_flag_file_path"
+    install_libretech_wiring_tool
 }
 
 main
