c1ba7053d5b1b63b64c0cfadad0beaa0ed876b5c
Juan Becerra <jdb9056@nyu.edu>
2024-02-05 12:28:35 -0500

    Remove unnecessary comments and add color coding

--- scripts/setup/post-reboot/connect_wifi.sh
+++ scripts/setup/post-reboot/connect_wifi.sh
@@ -34,21 +34,18 @@ connect_wifi() {
 
         if [ $status -eq 0 ]; then
             success=true
-            echo "Connected successfully to $WIFI_NAME."
+            echo "\033[0;32m[OK]\033[0m\t\tConnected successfully to $WIFI_NAME."
         elif [ $status -eq 1 ]; then
-            # Password incorrect
             input_credentials "Incorrect password. Please enter credentials again."
         elif [ $status -eq 10 ]; then
-            # No network found
             input_credentials "Please enter credentials again."
         else
-            # Other errors
             echo "An unexpected error occurred. Unable to connect."
         fi
     done
 
     if [ "$success" = false ]; then
-        echo "Failed to connect after $max_attempts attempts."
+        echo "\033[0;31m[FAIL]\033[0m\t\tFailed to connect after $max_attempts attempts."
     else
         nmcli connection modify "$WIFI_NAME" connection.autoconnect yes
     fi
@@ -57,8 +54,8 @@ connect_wifi() {
 main() {
     assert_conditions
 
+    local answer
     while true; do
-        local answer
         read -p "Do you wish to connect to WiFi? (Y/n)" user_input
         echo
         case "${answer}" in
@@ -66,7 +63,7 @@ main() {
                 # connect_wifi
                 break
                 ;;
-            [Nn] ) echo "Not connected to WiFi. Run this script if you wish to connect to WiFi.";
+            [Nn] ) echo "Run this script if you wish to connect to WiFi.";
                 break
                 ;;
             * )    echo "Invalid input. Please answer 'Y' (yes) or 'n' (no)."
