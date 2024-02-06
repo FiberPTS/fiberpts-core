c27d4ffed8805f953898caad12aa8dd1411ab856
Juan Becerra <jdb9056@nyu.edu>
2024-02-06 14:02:27 -0500

    Commit get_new_device_id and insert_device_data functions
    These two functions are designed to be used whenever a new device is
    being setup. The next available device ID is returned and then a
    new device record will be added with this new device ID.

--- src/cloud_db/cloud_db.py
+++ src/cloud_db/cloud_db.py
@@ -13,7 +13,6 @@ script_dir = os.path.dirname(script_path)
 
 load_dotenv(f"{script_dir}/../../.env")
 
-# TODO: Must first check if there is a record in devices table for the associated device_id and machine_name (can hardcode this)
 class CloudDBClient:
     """Client for interacting with a cloud database using the Supabase API."""
 
@@ -48,3 +47,50 @@ class CloudDBClient:
         }
         response = self.client.table('tap_data').insert(tap_record).execute()
         return response
+
+    def insert_device_data(self, device_id: str) -> APIResponse:
+        """Inserts a new device record into the 'devices' table.
+
+        Before insertion, it checks if the device ID already exists to avoid duplicates.
+        After insertion, the response can be used to confirm whether the device was successfully created.
+
+        Args:
+            device_id: A string representing the unique device ID to be inserted.
+
+        Returns:
+            An APIResponse object representing the result of the database operation.
+        """
+        # TODO: Check if device_id already exists
+        # TODO: How can I check whether the device id was created based on the response
+        device_record = {
+            'device_id': device_id,
+            'name': ''
+        }
+        response = self.client.table('devices').insert(device_record).execute()
+        return response
+
+    def get_new_device_id(self):
+        """Generates a new, unique device ID for a new device.
+
+        The function retrieves the highest current device ID from the 'devices'
+        table, increments it by one, and formats it to maintain consistent ID
+        structure. If no device IDs are present in the table, it starts
+        numbering from 'fpts-001'.
+
+        Returns:
+            A string representing the newly generated device ID.
+        """
+        response = self.client.table('devices') \
+            .select('device_id') \
+            .order('device_id', desc=True) \
+            .limit(1) \
+            .execute()
+
+        device_id = ''
+        if not response:
+            device_id = 'fpts-001'
+        else:
+            # Extract numeric part of device ID, increment it, and format it
+            id_num = '{:03d}'.format(int(response.data[0]['device_id'][-3:]) + 1)
+            device_id = 'fpts-' + id_num
+        return device_id
