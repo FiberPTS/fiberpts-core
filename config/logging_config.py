7e06dd3c6d5a2c290b2ec25fd6b2c9193d5caa6b
NxFerrara <nxferrara1981@gmail.com>
2024-02-07 12:05:25 -0500

    First iteration, needs work
    I am doing this so I can pull it from my laptop.

--- config/logging_config.py
+++ config/logging_config.py
@@ -1,1 +1,27 @@
 # Logging configurations
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'class': 'concurrent_log_handler.ConcurrentRotatingFileHandler',
+            'formatter': 'standard',
+            'filename': 'path/to/your/logfile.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'encoding': 'utf8',
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
