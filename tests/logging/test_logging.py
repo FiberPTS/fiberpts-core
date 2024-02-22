9ccec624a841ae6eb1c6f484356a352f3ece2931
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2024-02-22 18:14:26 -0500

    Use logging config in test_logging.py
    Instead of directly setting up the configurations in the python script, we are testing using the configuration file.

--- tests/logging/test_logging.py
+++ tests/logging/test_logging.py
@@ -1,24 +1,14 @@
 import logging
-import logging.handlers
+import logging.config
+import os
 
-
-def setup_logger(logger_name):
-    logger = logging.getLogger(logger_name)
-    time_format = "%Y-%m-%d %H:%M:%S"
-    file_handler = logging.handlers.WatchedFileHandler('tests/logging/test.log') #Using WatchedFileHandler to synchronize with logrotate
-    logFormat = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s] - %(message)s", datefmt=time_format) #Formatting can be changed to preference
-    file_handler.setFormatter(logFormat)
-    logger.addHandler(file_handler)
-    logger.setLevel(logging.DEBUG) #Lowest Level which allows for all levels of messages to be output
-
-
-
+project_dir = os.path.abspath(os.path.join(__file__, '../../..'))
 
 if __name__ == "__main__":
-    setup_logger(__name__)
-    screen_logger = logging.getLogger(__name__)
-    screen_logger.critical("Testing Critical")
-    screen_logger.error("Testing Error")
-    screen_logger.warning("Testing Warning")
-    screen_logger.info("Testing Info")
-    screen_logger.debug("Testing Debug")
+    logging.config.fileConfig(f'{project_dir}/config/logging.conf')
+    logger = logging.getLogger(f'{os.path.basename(__file__)}')
+    logger.critical("Testing Critical")
+    logger.error("Testing Error")
+    logger.warning("Testing Warning")
+    logger.info("Testing Info")
+    logger.debug("Testing Debug")
