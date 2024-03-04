ecc04fb2bd41db17dbefb19279a3c42370c1bc28
NxFerrara <42093581+NxFerrara@users.noreply.github.com>
2024-03-04 13:11:16 -0500

    Fixing setup script status message format
    Remove "\t" from all script status messages to make the spacing consistent.

--- scripts/setup/post_reboot/1_create_pipes.sh
+++ scripts/setup/post_reboot/1_create_pipes.sh
@@ -3,12 +3,12 @@
 assert_conditions() {
     # Root check
     if [ "$(id -u)" -ne 0 ]; then
-        echo -e "\t${WARNING_MSG} This script must be run as root. Please use sudo."
+        echo -e "${WARNING_MSG} This script must be run as root. Please use sudo."
         exit 1
     fi
 
     if [ -z "${PIPE_FOLDER_PATH}" ] || [ -z "${TOUCH_SENSOR_TO_SCREEN_PIPE}" ] || [ -z "${NFC_TO_SCREEN_PIPE}" ]; then
-        echo -e "\t${WARNING_MSG} Required environment variables PIPE_FOLDER_PATH, TOUCH_SENSOR_TO_SCREEN_PIPE, or NFC_TO_SCREEN_PIPE are not set."
+        echo -e "${WARNING_MSG} Required environment variables PIPE_FOLDER_PATH, TOUCH_SENSOR_TO_SCREEN_PIPE, or NFC_TO_SCREEN_PIPE are not set."
         exit 1
     fi
 }
@@ -17,17 +17,17 @@ create_fifo_pipes() {
     echo -e "Creating FIFO pipes..."
 
     if [ -p "${TOUCH_SENSOR_TO_SCREEN_PIPE}" ]; then
-        echo -e "\t${WARNING_MSG} FIFO pipe '${TOUCH_SENSOR_TO_SCREEN_PIPE}' already exists."
+        echo -e "${WARNING_MSG} FIFO pipe '${TOUCH_SENSOR_TO_SCREEN_PIPE}' already exists."
     else
         mkfifo "${TOUCH_SENSOR_TO_SCREEN_PIPE}"
-        echo -e "\t\t${OK_MSG} FIFO pipe '${TOUCH_SENSOR_TO_SCREEN_PIPE}' created."
+        echo -e "${OK_MSG} FIFO pipe '${TOUCH_SENSOR_TO_SCREEN_PIPE}' created."
     fi
              
     if [ -p "${NFC_TO_SCREEN_PIPE}" ]; then
-        echo -e "\t${WARNING_MSG} FIFO pipe '${NFC_TO_SCREEN_PIPE}' already exists."
+        echo -e "${WARNING_MSG} FIFO pipe '${NFC_TO_SCREEN_PIPE}' already exists."
     else
         mkfifo "${NFC_TO_SCREEN_PIPE}"
-        echo -e "\t\t${OK_MSG} FIFO pipe '${NFC_TO_SCREEN_PIPE}' created."
+        echo -e "${OK_MSG} FIFO pipe '${NFC_TO_SCREEN_PIPE}' created."
     fi
 }
 
