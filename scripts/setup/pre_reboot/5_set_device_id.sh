0be113a3703b350cc6dfdbe893590f29c8cf94d5
NxFerrara <nxferrara1981@gmail.com>
2024-03-05 10:22:36 -0500

    Squashed commit of the following:
    commit d078b20fc4373fd497337076a3e8017cd5e254df
    Author: Juan Becerra <jdb9056@nyu.edu>
    Date:   Mon Mar 4 21:31:15 2024 -0500
    
        Fix warning and inconsistencies in 5_set_device_id
    
        Mostly adding double-quotes to variable substitution statements to avoid
        globbing and word splitting. Separated declaration and assignment of
        variables in the same line to avoid masking exit status.
    
        Note: When you declare and assign a variable in the same command using
              command substitution, the exit status of the command gets
              overwritten. Only the exit status of the assignment (which is
              typically successful, returning 0) is available, not the exit
              status of the command inside the command substitution.
    
    commit 4641dc57c58b310dd45740d82fd28a5ffe6ecd25
    Author: Juan Becerra <jdb9056@nyu.edu>
    Date:   Mon Mar 4 19:01:28 2024 -0500
    
        Fix reference to DATABASE_URL
    
    commit a9074e3a8dcbbd37e4f4f67bb54bc6916de8a9ad
    Merge: 01af904 6b3f020
    Author: Juan Becerra <jdb9056@nyu.edu>
    Date:   Mon Mar 4 18:56:21 2024 -0500
    
        Remove unnecessary comments
    
    commit 01af9047e8e124e03e0cd8ab613dc1b9de398175
    Author: Juan Becerra <jdb9056@nyu.edu>
    Date:   Mon Mar 4 18:31:31 2024 -0500
    
        Refactor get_new_device_id to fetch first unallocated device ID
    
        Logic in get_new_device_id() was changed to fetch the first available
        (unallocated) device ID from the database instead of generating a new
        ID based on the last record's ID. This approach allows for repurposing
        device IDs that were once taken but have been freed by a device.
    
        In order to enable this behavior, an 'allocated' column was created in
        the 'devices' table. This column serves to identify whether a particular
        device ID was allocated. With this in mind, insert_device_id() was
        modified to include an 'allocated' field in the database insert query,
        marking the device as allocated upon insertion.
    
        As of this commit, this implementation does not handle race conditions.

--- scripts/setup/pre_reboot/5_set_device_id.sh
+++ scripts/setup/pre_reboot/5_set_device_id.sh
@@ -4,57 +4,61 @@
 set -e
 
 readonly TABLE=devices
-readonly COLUMN=device_id
-readonly SUPABASE_URL=https://qfgukdtejvguiqtlqfay.supabase.co
+readonly COLUMNS=device_id,allocated
 
+# TODO: Add support for race conditions
 get_new_device_id() {
-    # Get the last device ID in the database
-    local response=$(
-        curl "${SUPABASE_URL}/rest/v1/${TABLE}?select=${COLUMN}&order=${COLUMN}.desc&limit=1" \
+    local response
+    response=$(
+        curl "${DATABASE_URL}/rest/v1/${TABLE}?select=${COLUMNS}" \
         -H "apikey: ${DATABASE_API_KEY}" \
         -H "Authorization: Bearer ${DATABASE_API_KEY}" \
         -H "Content-Type: application/json"
     )
 
-    local device_id
-    if [ response = "[]" ]; then
-        device_id="fpts-001"
-    else
-        # Parse JSON and extract first device ID
-        last_id=$(echo ${response} | jq -r '.[0].device_id')
+    local device_id="fpts-"
+    local count=0
+    while read -r record; do
+        local is_allocated
+        is_allocated=$(echo "${record}" | jq -r '.allocated')
 
-        id_num=$(printf "%03d" $((10#${last_id: -3} + 1)))
-        device_id="fpts-${id_num}"
+        if [[ "${is_allocated}" == false ]]; then
+            # Assumes correct formatting is used in table records
+            device_id=$(echo "${record}" | jq -r '.device_id')
+            break
+        fi
+
+        count=$((count + 1))
+    done < <(echo "${response}" | jq -c '.[]')
+
+    if [[ ${device_id} == "fpts-" ]]; then
+        device_id+=$((count + 1))
     fi
 
-    echo ${device_id}
+    echo "${device_id}"
 }
 
-# TODO: Add retry in case device ID is already taken
-# TODO: Freeing up a device ID in case a device is re-imaged
-#   - Ordering of device ids should not be altered (first available device ID must be
-#     fetched instead of the last one)
-# TODO: Rename device-id to hostname-id, and create an 'allocated' (bool) column that indicates whether a certain device ID is taken or not
-# TODO: Get 'hostname-id' and 'allocated', iterate over them. Keep count of records. If allocated is false, then take the ID and assign it 
-# to device's hostname. If no available IDs are found, then use count to create new ID. 
 insert_device_id() {
     local device_id=$1
-    local response=$(
-        curl -X POST "${SUPABASE_URL}/rest/v1/${TABLE}" \
+    local response
+    response=$(
+        curl -X POST "${DATABASE_URL}/rest/v1/${TABLE}" \
         -H "apikey: ${DATABASE_API_KEY}" \
         -H "Authorization: Bearer ${DATABASE_API_KEY}" \
         -H "Content-Type: application/json" \
         -H "Prefer: return=minimal" \
-        -d "{ \""${COLUMN}"\": \""${device_id}"\" }"
+        -d "{ \"device_id\": \"${device_id}\", \"allocated\": true }"
     )
-    echo ${response}
+    echo "${response}"
 }
 
 main() {
-    local device_id=$(get_new_device_id)
+    local device_id
+    device_id=$(get_new_device_id)
     readonly device_id
+    
     insert_device_id "${device_id}"
-    hostnamectl set-hostname ${device_id}
+    hostnamectl set-hostname "${device_id}"
     return 0
 }
 
