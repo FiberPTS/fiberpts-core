7b50f0269ffcf02dabf4fc0d9ff73d21f72b7fb8
Juan Becerra <jdb9056@nyu.edu>
2024-03-06 20:53:18 -0500

    Check Exit Status of systemctl Commands

--- scripts/setup/post_reboot/3_init_services.sh
+++ scripts/setup/post_reboot/3_init_services.sh
@@ -47,26 +47,30 @@ function process_service_files() {
     local service_filename
     service_filename=$(basename "${template}")
 
-    # BUG: Service file is not created on line 26
     if [ ! -f "${SYSTEMD_DIR}/${service_filename}" ]; then
       envsubst < "${template}" > "${SYSTEMD_DIR}/${service_filename}"
       systemctl enable "${service_filename}" > /dev/null
-
       if [ "$?" -eq 0 ]; then
         echo "${OK} '${service_filename}' enabled"
       else
         echo "${FAIL} Failed to enable '${service_filename}'"
+        exit 1
       fi
     else
       envsubst < "${template}" > "${SYSTEMD_DIR}/${service_filename}"
       systemctl daemon-reload
-      systemctl restart "${service_filename}"
-
-      if [ "$?" -eq 0 ]; then
-        echo "${OK} '${service_filename}' updated"
-      else
-        echo "${FAIL} Failed to update '${service_filename}'"
+      if [ "$?" -ne 0 ]; then
+        echo "${FAIL} Failed to reload systemd daemon"
+        exit 1
       fi
+
+      systemctl restart "${service_filename}"
+      if [ "$?" -ne 0 ]; then
+        echo "${FAIL} Failed to restart '${service_filename}'"
+        exit 1
+      fi
+
+      echo "${OK} '${service_filename}' updated"
     fi
   done
 }
