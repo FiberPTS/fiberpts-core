e7465e8886668db9117dc50820ff57cf772f4059
Nicholas Xavier Ferrara <42093581+NxFerrara@users.noreply.github.com>
2024-04-09 20:25:43 -0400

    Update nfc_reader.py

--- src/nfc_reader/nfc_reader.py
+++ src/nfc_reader/nfc_reader.py
@@ -1,5 +1,12 @@
 import nfc
 
-if __name__ == '__main__':
-    clf = nfc.ContactlessFrontend()
-    print(clf.open('tty:AML6'))
+def main():
+    clf = nfc.ContactlessFrontend('tty:AML6:pn532')
+    if clf:
+        print("Device is connected")
+        clf.close()
+    else:
+        print("Failed to connect to the device")
+
+if __name__ == "__main__":
+    main()
