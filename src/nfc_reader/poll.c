72f8ca88b65dde92ebdd912bd01f87a4883d0a23
NxFerrara <nxferrara1981@gmail.com>
2024-04-11 17:17:46 -0400

    Rename nfc_reader.c -> poll.c

--- src/nfc_reader/poll.c
+++ src/nfc_reader/poll.c
@@ -27,32 +27,90 @@ static void stop_polling(int sig)
     }
 }
 
-// Function to print the usage of the program
-static void print_usage(const char *progname)
-{
-    printf("usage: %s [-v]\n", progname);
-    printf("  -v\t verbose display\n");
+uint8_t poll(){
+    signal(SIGINT, stop_polling);
+
+    // Define poll number and period for NFC device polling
+    const uint8_t uiPollNr = 20;
+    const uint8_t uiPeriod = 2;
+    // Define modulation settings for polling
+    const nfc_modulation nmModulations[5] = {
+        {.nmt = NMT_ISO14443A, .nbr = NBR_106},
+        {.nmt = NMT_ISO14443B, .nbr = NBR_106},
+        {.nmt = NMT_FELICA, .nbr = NBR_212},
+        {.nmt = NMT_FELICA, .nbr = NBR_424},
+        {.nmt = NMT_JEWEL, .nbr = NBR_106},
+    };
+    const size_t szModulations = 5; // Number of modulations
+
+    nfc_target nt; // NFC target structure
+    int res = 0;   // Result of NFC operations
+
+    // Initialize libnfc context
+    nfc_init(&context);
+    if (context == NULL)
+    {
+        ERR("Unable to init libnfc (malloc)");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open NFC device
+    pnd = nfc_open(context, NULL);
+    if (pnd == NULL)
+    {
+        ERR("%s", "Unable to open NFC device.");
+        nfc_exit(context);
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize NFC device as initiator
+    if (nfc_initiator_init(pnd) < 0)
+    {
+        nfc_perror(pnd, "nfc_initiator_init");
+        nfc_close(pnd);
+        nfc_exit(context);
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO: Make this a loop to poll indefinitely, so use passive polling
+    // Start polling for NFC targets
+    if ((res = nfc_initiator_poll_target(pnd, nmModulations, szModulations, uiPollNr, uiPeriod, &nt)) < 0)
+    {
+        nfc_perror(pnd, "nfc_initiator_poll_target");
+        nfc_close(pnd);
+        nfc_exit(context);
+        return 1;
+    }
+
+    // Handle polling result
+    if (res > 0)
+    {
+        nfc_close(pnd);
+        nfc_exit(context);
+        return nt.nti.nai.abtUid;
+    }
+    else
+    {
+        printf("No target found.\n");
+    }
+
+    // Wait for card removal
+    printf("Waiting for card removing...");
+    while (0 == nfc_initiator_target_is_present(pnd, NULL))
+    {
+    }
+    nfc_perror(pnd, "nfc_initiator_target_is_present");
+    printf("done.\n");
+
+    // Cleanup
+    nfc_close(pnd);
+    nfc_exit(context);
+    return 1;
 }
 
 int main(int argc, const char *argv[])
 {
-    bool verbose = false;
-
     signal(SIGINT, stop_polling);
-
-    // Check command line arguments for verbose flag
-    if (argc != 1)
-    {
-        if ((argc == 2) && (0 == strcmp("-v", argv[1])))
-        {
-            verbose = true;
-        }
-        else
-        {
-            print_usage(argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
 
     // Define poll number and period for NFC device polling
     const uint8_t uiPollNr = 20;
