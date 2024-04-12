a7a8d7b7c9accf73666d91d93d50afabe4abe56b
NxFerrara <nxferrara1981@gmail.com>
2024-04-11 21:10:44 -0400

    Update main to call poll

--- src/nfc_reader/wrapper_test.py
+++ src/nfc_reader/wrapper_test.py
@@ -4,9 +4,9 @@ lib = CDLL("./poll.so")
 lib.poll.argtypes = [c_char_p, c_size_t]
 
 uid_len = 64
-uid_str = create_string_buffer(uid_len)
 
 while True:
-    lib.poll(uid_str, 64)
+    uid_str = create_string_buffer(uid_len)
+    lib.poll(uid_str, uid_len)
     print("UID String:", uid_str.value.decode('utf-8'))
 
