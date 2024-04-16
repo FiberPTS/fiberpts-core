cc3c4ed614c88001b2ec81039c25268cd6e3bc39
Nicholas Xavier Ferrara <42093581+NxFerrara@users.noreply.github.com>
2024-04-15 22:26:59 -0400

    Create no_wifi.py

---
+++ src/screen/no_wifi.py
@@ -0,0 +1,25 @@
+from src.screen.screen import Screen
+from src.utils.screen_utils import get_image_center
+
+def draw_no_wifi(screen: Screen) -> None:
+        """Draw the dashboard interface on the screen.
+        
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        screen.create_image(screen.dashboard_attributes.dashboard_bg_color)
+        image_center = get_image_center(screen.image)
+        screen.add_text(f"No WiFi Connection",
+                      image_center,
+                      screen.dashboard_attributes.dashboard_font_family,
+                      screen.dashboard_attributes.dashboard_font_size,
+                      screen.dashboard_attributes.dashboard_font_color,
+                      centered=True)
+        screen.draw_image()
+
+if __name__ == '__main__':
+    screen = Screen()
+    draw_no_wifi(screen)
