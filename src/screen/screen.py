84d4d64d2da875eb55a33c11a24bf3b5c2491026
NxFerrara <nxferrara1981@gmail.com>
2024-04-23 18:35:21 -0400

    Add temporary fix to partial frame

--- src/screen/screen.py
+++ src/screen/screen.py
@@ -87,6 +87,9 @@ class Screen:
             self.image = self.image.rotate(90, expand=True)
             write_image_to_fb(self.image, self.display_attributes.display_fb_path,
                               self.display_attributes.display_fb_lock_path)
+            time.sleep(self.display_attributes.display_frame_rate)
+            write_image_to_fb(self.image, self.display_attributes.display_fb_path,
+                              self.display_attributes.display_fb_lock_path)
 
     def add_text(self,
                  text: str,
